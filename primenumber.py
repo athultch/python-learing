2b1b6b700ef35a1d8429a3c5b9b871ff4a587589
athultch <athultch@gmail.com>
2023-11-12 22:22:25 +0530

    add two files

--- primenumber.py
+++ primenumber.py
@@ -1,5 +1,11 @@
-num =int(input("enter a number ="))
+num = int(input("Enter a number: "))
 
 if num == 1:
-   print ("it isnot a prime number")
-   
+    print("It is not a prime number")
+elif num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("It is not a prime number")
+            break
+    else:
+        print("It is a prime number")
