ef37f2c4eaf527db70b0e35fb2c566ab8d0148e2
athultch <athultch@gmail.com>
2023-11-14 22:15:17 +0530

    mulipical addd

---
+++ multiplicationwhileloop.py
@@ -0,0 +1,7 @@
+n = int(input("enter the number : "))  # Takes user input for the number
+
+i = 1  # Initializes a variable i to 1
+
+while i <= n:
+    print(n, "x", i, "=", n * i)  # Prints the multiplication table entry
+    i = i + 1  # Increments the value of i for the next iteration
